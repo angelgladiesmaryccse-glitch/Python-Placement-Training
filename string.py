053ef03cca6c6d0f1a0725acf3a2dd109ef0070f
angelgladiesmaryccse-glitch <angelgladiesmary.c.cse@care.ac.in>
2025-09-27 11:51:54 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,10 +1,31 @@
-n=int(input())
-n=2*n-1
-for r in range(1,n+1):
-    for c in range(1,n+1):
-        if(r==c or r+c==n+1):
-            print("*",end="")
-        else:
-            print(" ",end="")
-    print()
+#anagram 
+
+a=input().strip()
+b=input().strip()
+if sorted(a)==sorted(b):
+    print(a,"and",b,"are Anagrams.")
+else:
+    print(a,"and",b,"are Not Anagrams.")
+
+#Isogram
+
+a=input()
+s=sorted(a)
+flag=0
+for i in range(len(s)-1):
+    if s[i]==s[i+1]:
+        flag=1
+if flag==0:
+    print("ISOGRAM")
+else:
+    print("NOT ISOGRAM")
     
+# convert the digits of the string into a single number
+
+a=input()
+digits= ''.join([i for i in a if i.isdigit()])
+if digits:
+    number=int(digits)
+else:
+    number= 0
+print (number)
